b4299c61eb921457b4a7ec3f04bf7e028e243ddc
rishabhtatia <rishabh.tatia@codeastu.com>
2020-12-05 11:16:46 +0530

    Initial commit after some input form changes

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
+import Backdrop from "../../UI/Backdrop/Backdrop";
+import Aux from "../../../hoc/Auxillary/Auxilary";
+import styles from "./SideDrawer.module.css";
+const sideDrawer = (props) => {
+  let attachedClasses = [styles.SideDrawer, styles.Close];
+  if (props.open) {
+    attachedClasses = [styles.SideDrawer, styles.Open];
+  }
+  return (
+    <Aux>
+      <Backdrop show={props.open} clicked={props.closed} />
+      <div className={attachedClasses.join(" ")}>
+        <div className={styles.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
+      </div>
+    </Aux>
+  );
+};
+
+export default sideDrawer;
