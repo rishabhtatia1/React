66c6cb1300ccf2a0cf28f72781030ec0aa3cae9d
rishabhtatia <rishabh.tatia@codeastu.com>
2020-12-05 11:55:53 +0530

    Formatting and Linter changes

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,27 +1,27 @@
-import React from 'react';
+import React from "react";
 
-import styles from './Burger.module.css';
-import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+import styles from "./Burger.module.css";
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
-const burger = ( props ) => {  
-    let transformedIngredients = Object.keys(props.ingredients)
+const burger = (props) => {
+  let transformedIngredients = Object.keys(props.ingredients)
     .map((igKey) => {
-        return [...Array(props.ingredients[igKey])].map((_, idx) => {
-            return <BurgerIngredient key={igKey + idx} type={igKey}/>;
-        })    
+      return [...Array(props.ingredients[igKey])].map((_, idx) => {
+        return <BurgerIngredient key={igKey + idx} type={igKey} />;
+      });
     })
     .reduce((arr, el) => arr.concat(el), []);
-    if (transformedIngredients.length === 0) {
-        transformedIngredients = <p>Please start adding ingredients!!!</p>
-    }
-    console.log(transformedIngredients);        
-    return (
-        <div className={styles.Burger}>
-            <BurgerIngredient type="bread-top" />            
-            {transformedIngredients}
-            <BurgerIngredient type="bread-bottom" />
-        </div>
-    );
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!!!</p>;
+  }
+  console.log(transformedIngredients);
+  return (
+    <div className={styles.Burger}>
+      <BurgerIngredient type="bread-top" />
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
 };
 
 export default burger;
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import burgerLogo from '../../assets/images/burger-logo.png'
-import styles from './Logo.module.css';
+import React from "react";
+import burgerLogo from "../../assets/images/burger-logo.png";
+import styles from "./Logo.module.css";
 
 const logo = (props) => (
-    <div className={styles.Logo} style={{height: props.height}}>
-        <img src={burgerLogo} alt="MYBURGER"></img>
-    </div>
-
+  <div className={styles.Logo} style={{ height: props.height }}>
+    <img src={burgerLogo} alt="MYBURGER"></img>
+  </div>
 );
 
 export default logo;
